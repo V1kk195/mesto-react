0c14ce0572de622a1f0ee72dc32fbfeabf9dd67f
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-11-12 23:48:56 +0300

    moved popups initiating in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from "./header/Header";
 import Main from "./main/Main";
+import Popup from "./popup/Popup";
 
 
 function App() {
@@ -15,15 +16,10 @@ function App() {
 
     const handleEditProfileClick = () => {
         setIsEditProfilePopupOpen(true);
-        console.log('edit profile clicked');
-        // document.querySelector('.popup_type_user-profile').classList.add('popup_is-opened');
     }
 
     const handleAddPlaceClick = () => {
         setIsAddPlacePopupOpen(true);
-        console.log('add card clicked');
-
-        // document.querySelector('.popup_type_new').classList.add('popup_is-opened');
     }
 
     return (
@@ -33,9 +29,25 @@ function App() {
               onEditProfile={handleEditProfileClick}
               onAddPlace={handleAddPlaceClick}
               onEditAvatar={handleEditAvatarClick}
-              isEditProfilePopupOpen={isEditProfilePopupOpen}
-              isAddPlacePopupOpen={isAddPlacePopupOpen}
-          />
+          >
+              <Popup title="Редактировать профиль" name="user-profile" isOpen={isEditProfilePopupOpen}>
+                  <input type="text" name="username" id="username" required className="popup__input popup__input_type_username field-not-clickable" placeholder="Имя" />
+                  <span className="error-message" id="error-username"></span>
+                  <input type="text" name="about" id="about" required className="popup__input popup__input_type_about field-not-clickable" placeholder="О себе" />
+                  <span className="error-message" id="error-about"></span>
+                  <button type className="edit-profile__button field-not-clickable" id="edit-profile__button">Сохранить</button>
+              </Popup>
+
+              <Popup title="Новое место" name="new" isOpen={isAddPlacePopupOpen}>
+                  <input type="text" name="name" id="imgname" className="popup__input popup__input_type_name field-not-clickable" placeholder="Название" />
+                  <span className="error-message" id="error-imgname" />
+                  <input type="URL" name="link" id="link" className="popup__input popup__input_type_link-url field-not-clickable" placeholder="Ссылка на картинку" />
+                  <span className="error-message" id="error-link" />
+                  <button type className="button popup__button field-not-clickable">+</button>
+              </Popup>
+
+          </Main>
+
         </>
     );
 }
