ab328aa78df1fd995134aa13aad4081c86c97e30
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-11-13 18:54:52 +0300

    added get user info api call in Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import Popup from "../popup/Popup";
+import Api from '../../utils/api';
 
 function Main(props) {
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
+
+    React.useEffect(() => {
+        Api.getUserInfo()
+            .then(data => {
+                if(!data.name) return Promise.reject('data.message');
+                setUserName(data.name);
+                setUserDescription(data.about);
+                setUserAvatar(data.avatar);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            })
+    }, []);
+
     return (
         <main>
             <div className="profile root__section">
                 <div className="user-info">
-                    <div className="user-info__photo"></div>
+                    <div className="user-info__photo" style={{ backgroundImage: `url(${userAvatar})` }}></div>
                     <div className="user-info__data">
-                        <h1 className="user-info__name"></h1>
-                        <p className="user-info__job"></p>
+                        <h1 className="user-info__name">{userName}</h1>
+                        <p className="user-info__job">{userDescription}</p>
                         <button className="button user-info__edit-button" onClick={props.onEditProfile}>Edit</button>
                     </div>
 
