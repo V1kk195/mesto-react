dfae78b274832717a023769df1fe04430b13722c
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-11-16 12:55:00 +0300

    Created CurrentUserContext and added it to App

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Main from "./main/Main";
 import Popup from "./popup/Popup";
 import ImagePopup from "./imagePopup/ImagePopup";
 import api from '../utils/api';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
                 console.log(err);
                 return err;
             })
-    })
+    }, [])
 
     const handleEditAvatarClick = () => {
         setIsEditAvatarPopupOpen(true);
@@ -51,33 +52,34 @@ function App() {
 
     return (
         <>
-          <Header />
-          <Main
-              onEditProfile={handleEditProfileClick}
-              onAddPlace={handleAddPlaceClick}
-              onEditAvatar={handleEditAvatarClick}
-              onCardClick={handleCardClick}
-          >
-              <Popup title="Редактировать профиль" name="user-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-                  <input type="text" name="username" id="username" required className="popup__input popup__input_type_username field-not-clickable" placeholder="Имя" />
-                  <span className="error-message" id="error-username"></span>
-                  <input type="text" name="about" id="about" required className="popup__input popup__input_type_about field-not-clickable" placeholder="О себе" />
-                  <span className="error-message" id="error-about"></span>
-                  <button type className="edit-profile__button field-not-clickable" id="edit-profile__button">Сохранить</button>
-              </Popup>
+            <CurrentUserContext.Provider value={currentUser}>
+                <Header />
+                <Main
+                    onEditProfile={handleEditProfileClick}
+                    onAddPlace={handleAddPlaceClick}
+                    onEditAvatar={handleEditAvatarClick}
+                    onCardClick={handleCardClick}
+                >
+                    <Popup title="Редактировать профиль" name="user-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+                        <input type="text" name="username" id="username" required className="popup__input popup__input_type_username field-not-clickable" placeholder="Имя" />
+                        <span className="error-message" id="error-username"></span>
+                        <input type="text" name="about" id="about" required className="popup__input popup__input_type_about field-not-clickable" placeholder="О себе" />
+                        <span className="error-message" id="error-about"></span>
+                        <button type className="edit-profile__button field-not-clickable" id="edit-profile__button">Сохранить</button>
+                    </Popup>
 
-              <Popup title="Новое место" name="new" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
-                  <input type="text" name="name" id="imgname" className="popup__input popup__input_type_name field-not-clickable" placeholder="Название" />
-                  <span className="error-message" id="error-imgname" />
-                  <input type="URL" name="link" id="link" className="popup__input popup__input_type_link-url field-not-clickable" placeholder="Ссылка на картинку" />
-                  <span className="error-message" id="error-link" />
-                  <button type className="button popup__button field-not-clickable">+</button>
-              </Popup>
+                    <Popup title="Новое место" name="new" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+                        <input type="text" name="name" id="imgname" className="popup__input popup__input_type_name field-not-clickable" placeholder="Название" />
+                        <span className="error-message" id="error-imgname" />
+                        <input type="URL" name="link" id="link" className="popup__input popup__input_type_link-url field-not-clickable" placeholder="Ссылка на картинку" />
+                        <span className="error-message" id="error-link" />
+                        <button type className="button popup__button field-not-clickable">+</button>
+                    </Popup>
 
-              <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+                    <ImagePopup card={selectedCard} onClose={closeAllPopups} />
 
-          </Main>
-
+                </Main>
+            </CurrentUserContext.Provider>
         </>
     );
 }
