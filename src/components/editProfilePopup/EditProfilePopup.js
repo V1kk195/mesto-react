4d1117a8515edf4b4c906e2002827123e0718df8
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-11-17 17:46:30 +0300

    Added controlled components to the form in EditProfilePopup

--- src/components/editProfilePopup/EditProfilePopup.js
+++ src/components/editProfilePopup/EditProfilePopup.js
@@ -2,11 +2,24 @@ import React from 'react';
 import PopupWithForm from "../popupWithForm/PopupWithForm";
 
 function EditProfilePopup(props) {
+    const [name, setName] = React.useState('');
+    const [description, setDescription] = React.useState('');
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value);
+    }
+
     return (
         <PopupWithForm title="Редактировать профиль" name="user-profile" isOpen={props.isOpen} onClose={props.onClose}>
-            <input type="text" name="username" id="username" required className="popup__input popup__input_type_username field-not-clickable" placeholder="Имя" />
+            <input type="text" value={name} name="username" id="username" required className="popup__input popup__input_type_username field-not-clickable"
+                   placeholder="Имя" onChange={handleNameChange} />
             <span className="error-message" id="error-username" />
-            <input type="text" name="about" id="about" required className="popup__input popup__input_type_about field-not-clickable" placeholder="О себе" />
+            <input type="text" value={description} name="about" id="about" required className="popup__input popup__input_type_about field-not-clickable"
+                   placeholder="О себе" onChange={handleDescriptionChange} />
             <span className="error-message" id="error-about" />
             <button type="button" className="edit-profile__button field-not-clickable" id="edit-profile__button">Сохранить</button>
         </PopupWithForm>
