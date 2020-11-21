91c8c56bd168f85c5cb5ab5cc06de95d183ec8af
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-11-21 23:36:18 +0300

    made new validation of Edit Profile form

--- src/components/editProfilePopup/EditProfilePopup.js
+++ src/components/editProfilePopup/EditProfilePopup.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PopupWithForm from "../popupWithForm/PopupWithForm";
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import formValidator from "../../utils/formValidator";
-
 
 function EditProfilePopup(props) {
+    const currentUser = React.useContext(CurrentUserContext);
     const [name, setName] = React.useState('');
     const [description, setDescription] = React.useState('');
-    const currentUser = React.useContext(CurrentUserContext);
     const buttonRef = React.useRef();
-
+    const [errors, setErrors] = React.useState({});
+    const [formValid, setFormValid] = React.useState(false);
 
     React.useEffect(() => {
         setName(currentUser.name || '');
         setDescription(currentUser.about || '');
-        const validationUser = new formValidator(document.forms['user-profile'], buttonRef.current);
-        validationUser.setEventListeners();
-        validationUser.setSubmitButtonState();
     }, [currentUser])
+
+    React.useEffect(() => {
+        validateForm();
+    }, [name, description])
 
     const handleNameChange = (e) => {
         setName(e.target.value);
@@ -38,15 +38,41 @@ function EditProfilePopup(props) {
         props.onUpdateUser(name, description);
     }
 
+    const validateField = (fieldName, fieldValue) => {
+        if(fieldValue.trim() === '') {
+            setErrors({...errors, [fieldName]: 'Это обязательное поле'});
+            return false;
+        }
+        if(!(fieldValue.trim().length > 1 && fieldValue.trim().length < 30)) {
+            setErrors({...errors, [fieldName]: 'Должно быть от 2 до 30 символов'});
+            return false;
+        }
+        setErrors({...errors, [fieldName]: ''});
+        return true;
+    }
+
+    const validateForm = () => {
+        if(validateField('name', name) && validateField('description', description)) {
+            return setFormValid(true);
+        }
+        return setFormValid(false);
+    }
+
+
     return (
         <PopupWithForm title="Редактировать профиль" name="user-profile" isOpen={props.isOpen} onClose={handleClose} onSubmit={handleSubmit} >
             <input type="text" value={name} name="username" id="username" required className="popup__input popup__input_type_username field-not-clickable"
                    placeholder="Имя" onChange={handleNameChange} />
-            <span className="error-message" id="error-username" />
+            <span className="error-message" id="error-username">
+                {errors.name || ''}
+            </span>
             <input type="text" value={description} name="about" id="about" required className="popup__input popup__input_type_about field-not-clickable"
                    placeholder="О себе" onChange={handleDescriptionChange} />
-            <span className="error-message" id="error-about" />
-            <button ref={buttonRef} type="submit" className="edit-profile__button popup__button field-not-clickable" id="edit-profile__button">Сохранить</button>
+            <span className="error-message" id="error-about">
+                {errors.description || ''}
+            </span>
+            <button ref={buttonRef} type="submit" className={`edit-profile__button popup__button field-not-clickable ${formValid && "popup__button_active"}`}
+                    id="edit-profile__button" disabled={!formValid} >Сохранить</button>
         </PopupWithForm>
     )
 }
